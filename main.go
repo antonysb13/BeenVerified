45faea53e25c19f1993d6f2f252523c6e681d43d
antonysb13 <antonysb13@gmail.com>
2017-02-11 23:37:02 -0600

    main file has been organized in different functions for a clearer understanding

--- main.go
+++ main.go
@@ -13,8 +13,14 @@ import (
     _ "github.com/mattn/go-sqlite3"
 )
 
-/* Model */
+/* Constants */
 
+//File path of the database
+const databaseFilePath = "./jrdd.db"
+
+/* Models */
+
+//Song 
 type Song struct{
 	ID int 
 	Artist string
@@ -23,15 +29,18 @@ type Song struct{
 	Length int
 }
 
+//Array of Songs
 type SongsList struct{
 	Songs []Song
 }
 
+//Genre
 type Genre struct{
 	ID int
 	Name string
 }
 
+//Array of Genres
 type GenresList struct{
 	Genres []Genre
 }
@@ -42,77 +51,99 @@ func main() {
 
 	fmt.Println("Server starts ...")
 
+	//Handlers
 	mux := goji.NewMux()
+
+	//Songs Handlers
 	mux.HandleFunc(pat.Get("/songs"), findAllSongs)
-
+	/*mux.HandleFunc(pat.Get("/songs/artist/:artist"), findSongByArtist)
+	mux.HandleFunc(pat.Get("/songs/song/:song"), findSongBySong)
+	mux.HandleFunc(pat.Get("/songs/genre/:genre"), findSongByGenre)
+	mux.HandleFunc(pat.Get("/songs/length/:minLength/:maxLength"), findSongByLength)
+	
+	//Genres Handlers
+	mux.HandleFunc(pat.Get("/genres"), findAllGenres)
+	*/
+	//Host and port of the server
 	http.ListenAndServe("localhost:8080", mux)
-
 }
-
-/* Database Functions */
-
-
 
 /* Auxiliary Functions */
 
+//findAllSongs finds all the songs in the database
 func findAllSongs(w http.ResponseWriter, r *http.Request){
 
-	database, databaseError := sql.Open("sqlite3", "./jrdd.db")
+	//Initilize and open the database
+	database := initDatabase(databaseFilePath)
+	defer database.Close()
 
-	if databaseError != nil {
-        fmt.Println("Something went wrong openning the database.")
-        fmt.Println(databaseError)
-    }
-    defer database.Close()
-
-    sqlStatement := "SELECT S.ID, S.artist, S.song, G.name, S.length FROM songs as S INNER JOIN genres as G on S.genre = G.ID"
-
-    rows, rowsError := database.Query (sqlStatement)
-
-    if rowsError != nil {
-    	fmt.Println("Something went wrong executing the sql statement.")
-        fmt.Println(rowsError)
-    }
+	//Get all songs in database
+    rows := findAllSongsDB(database)
     defer rows.Close()
  
     songs := []Song {}
 
+    //Iterate over the returned rows
     for rows.Next(){
-    	var id int
-    	var artist string
-    	var song string
-    	var genre string
-    	var length int
+
+    	song := Song{}
+
     	songError := rows.Scan(
-    		&id, 
-    		&artist,
-    		&song,
-    		&genre,
-    		&length)
+    		&song.ID, 
+    		&song.Artist,
+    		&song.Song,
+    		&song.Genre,
+    		&song.Length)
 
     	if songError != nil{
     		fmt.Println("Something went wrong trying to get a song.")
         	fmt.Println(songError)
     	}
 
-    	songAuxiliar := Song{
-    		ID: id,
-    		Artist: artist,
-    		Song: song,
-    		Genre: genre,
-    		Length: length}
-
-    	songs = append(songs, songAuxiliar)
+    	songs = append(songs, song)
     }
 
     songsListResult := SongsList {
     	Songs: songs,
     }
 
+    //Encode the Go object into JSON data
     jsonResponse,_ := json.Marshal(songsListResult)
 
     fmt.Println(string(jsonResponse))
 
+    //Format the JSON into string and write the result to w  
     fmt.Fprintf(w, string(jsonResponse))
 }
+
+/* Database Functions */
+
+//initDatabase initializes and opens the database located in the given filePath
+func initDatabase(filePath string) *sql.DB{
+	database, databaseError := sql.Open("sqlite3", filePath)
+
+	if databaseError != nil {
+        fmt.Println("Something went wrong openning the database: " + filePath)
+        fmt.Println(databaseError)
+    }
+
+    return database 
+}
+
+//findAllSongsDB gets all songs in database by executing a sql statement
+func findAllSongsDB(database *sql.DB) *sql.Rows{
+	sqlStatement := "SELECT S.ID, S.artist, S.song, G.name, S.length FROM songs as S INNER JOIN genres as G on S.genre = G.ID"
+
+    //Execute the sql statement
+    rows, rowsError := database.Query(sqlStatement)
+
+    if rowsError != nil {
+    	fmt.Println("Something went wrong executing the sql statement: " + sqlStatement)
+        fmt.Println(rowsError)
+    }
+
+    return rows
+}
+
+
 
